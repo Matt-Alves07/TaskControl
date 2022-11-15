0bdf85b4864133afc015f9fe20925a492b3739f7
Matheus Alves <mathalves07.winstore@outlook.com>
2022-11-15 12:19:29 -0300

    Included Entity Framework Core reference, and created GET and POST paths for tasks

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(option => option.UseInMemoryDatabase("taskdb"));
 
 var app = builder.Build();
 
@@ -14,6 +18,30 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//Get all tasks
+app.MapGet("/tasks", async (AppDbContext db) => await db.Tasks.ToListAsync());
+//Create a new task
+app.MapPost("/tasks", async(Task task, AppDbContext db) =>
+{
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{task.Id}", task);
+});
+
 app.UseHttpsRedirection();
 
 app.Run();
+
+class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public Boolean IsFinished { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Task> Tasks => Set<Task>();
+}
